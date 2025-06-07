907d47f05c1abb4afb723ad31cd3a0133ab3ecf4
anqixxx <anqix2002@gmail.com>
2025-06-07 11:00:59 +0900

    Tranliteration Prototype with unidecode

--- prototype.py
+++ prototype.py
@@ -1,16 +1,45 @@
 # Proof of concept of transliteration using Nominatim as a library
 
 import nominatim_api as napi
-
+from unidecode import unidecode
 import asyncio
 
 async def search(query):
     async with napi.NominatimAPIAsync() as api:
-        return await api.search(query)
+        return await api.search(query, address_details=True)
+        # return await api.search(query)
 
-variable = 'yi yuan'
+def transliterate(text): # defaults to english
+    return unidecode(text)
+
+variable = 'hospital in dandong'
 results = asyncio.run(search(f"{variable}"))
+
 if not results:
     print(f'Cannot find {variable}')
 else:
     print(f'Found a place at {results[0].centroid.x},{results[0].centroid.y}')
+    
+    print('\nOriginal Result: ')
+    locale = napi.Locales(['chinese']) 
+    for i, result in enumerate(results):
+        address_parts = result.address_rows.localize(locale)
+        print(f"{i + 1}. {', '.join(address_parts)}")
+
+    locale = napi.Locales(['en']) 
+    print('\nDirect Localization to English:')
+    for i, result in enumerate(results):
+        address_parts = result.address_rows.localize(locale)
+        print(f"{i + 1}. {', '.join(address_parts)}")
+
+    print('\nTranliterated Result: ')
+    locale = napi.Locales(['chinese']) 
+    for i, result in enumerate(results):
+        address_parts = result.address_rows.localize(locale)
+        print(f"{i + 1}. {', '.join(unidecode(part) for part in address_parts)}")
+
+    print('\n Combination Result: ')
+    locale = napi.Locales(['en']) 
+    for i, result in enumerate(results):
+        address_parts = result.address_rows.localize(locale)
+        print(f"{i + 1}. {', '.join(unidecode(part) for part in address_parts)}")
