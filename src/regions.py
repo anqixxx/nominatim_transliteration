fedc6085c1455d374d3ae64093b88ceee96bc700
anqixxx <anqix2002@gmail.com>
2025-07-16 10:39:11 -0400

    dont understand why this is not working, does lookup on api do something different?

--- src/regions.py
+++ src/regions.py
@@ -9,14 +9,27 @@ async def search(query):
         return await api.search(query, address_details=True)
         # return await api.search(query)
 
+
 async def get_hk_geojson():
     """ Nominatim Search Query modified for Hong Kong
     """
     async with napi.NominatimAPIAsync() as api:
-        # result = await api.lookup(places="R913110", polygon_geojson=True)
-        result = await api.lookup("R913110")
+        # result = await api.lookup(places="R913110")
+        result = await api.lookup(["R913110"])
         print(result)
-        # return result[0].geometry["geojson"]
+        return result
+
+# async def lookup(self, places: Sequence[ntyp.PlaceRef], **params: Any) -> SearchResults:
+#     """ Get simple information about a list of places.
+
+#         Returns a list of place information for all IDs that were found.
+#     """
+#     details = ntyp.LookupDetails.from_kwargs(params)
+#     async with self.begin() as conn:
+#         conn.set_query_timeout(self.query_timeout)
+#         if details.keywords:
+#             await nsearch.make_query_analyzer(conn)
+#         return await get_places(conn, places, details)
 
 hk = asyncio.run(get_hk_geojson())
 print(hk)
